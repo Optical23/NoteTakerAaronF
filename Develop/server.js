8eb45431d1d711ac6ec6becc39c154ac554c6c7f
Optical23 <aaroneferguson@gmail.com>
2022-01-04 13:00:20 -0600

    intial files plus basic server

---
+++ Develop/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+const app = express();
+const PORT = 3001;
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'))
+});
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'))
+
+    console.info(`${req.method} request received to get notes`);
+});
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
