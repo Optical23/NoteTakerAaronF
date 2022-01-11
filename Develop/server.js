0eae709f3443d74479c98fa1be4967b42fc1dd0a
Optical23 <40125489+Optical23@users.noreply.github.com>
2022-01-11 14:04:30 -0600

    api/notes request added to the server

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const db = require('./db/db.json');
+const { response } = require('express');
+const uuid = require('../../05-Data-Persistence/Unsolved/helpers/uuid');
 
 const app = express();
 const PORT = 3001;
@@ -13,6 +16,50 @@ app.get('/notes', (req, res) => {
 
     console.info(`${req.method} request received to get notes`);
 });
+
+app.get('/api/notes', (req, res) => {
+  // for(let i = 0; i < db.length; i++){
+  //   let notes = [];
+  //   notes.push(db[i]);
+  // }
+  for (let i = 0; i < db.length; i++) {
+    const element = array[i];
+    
+    
+  }
+  res.json(db[0]);
+});
+
+app.post('/api/notes', (req, res) => {
+  
+  const { title, text } = req.body;
+  if (title && text){
+    const newNote = {
+      title,
+      text,
+      note_id: uuid(),
+    }
+  
+
+  const noteString = JSON.stringify(newNote);
+
+  fs.writeFile(`./db/db.json`, noteString, (err) => {
+    err ? console.error(err) : console.log(`Review for ${newNote.product} has been posted`);
+  })
+
+  const response = {
+    status: 'success',
+    body: newNote,
+  };
+
+  console.log(response);
+  res.json(response);
+  }else {
+    res.json('Error in posting.');
+  }
+});
+
+// app.post('/notes')
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
